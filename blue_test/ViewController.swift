1db4627e3ca2552e15000a6cb96d6f43807f7204
Yurii Topchii <yurii@nimbla.com>
2019-04-02 20:07:09 +0300

    Initial Commit

---
+++ blue_test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  blue_test
+//
+//  Created by Yurii Topchii on 4/2/19.
+//  Copyright © 2019 Yurii Topchii. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
